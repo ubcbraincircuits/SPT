39101dfdd75ccab6ba6a3925bbfe3575eb7203e3
tf4ong <53628964+tf4ong@users.noreply.github.com>
2019-11-08 11:07:42 -0800

    Add files via upload

---
+++ SPT_script.sh
@@ -0,0 +1,91 @@
+                             
+
+#!/bin/bash
+# This script should completely set up SPT on a blank Raspberry pi.
+#$ sudo chmod 777 AFH_script.sh
+#$ ./AFH_script.sh
+
+mkdir SPT
+cd SPT
+
+
+#updates pi, bypassing y/n prompts, clears out unused packages
+sudo apt-get update && sudo apt-get upgrade -y && apt-get autoremove && apt-get autoclean
+
+
+
+echo "cloning pulsedThread and building makefile"
+git clone https://github.com/jamieboyd/pulsedThread.git
+cd pulsedThread
+sudo make
+sudo make install
+python3 setup_ptGreeter.py install
+python3 setup_pyPTpyFuncs.py install
+cd ..
+
+
+
+echo "Cloning GPIO_Thread"
+git clone https://github.com/jamieboyd/GPIO_Thread.git
+cd GPIO_Thread
+python3 HX711_setup.py install # is this neccesary??
+
+python3 SimpleGPIO_setup.py install
+python3 StepperMotor_setup.py install 
+python3 leverThread_setup.py install 
+python3 PWM_thread_setup.py install
+cd ..
+
+
+echo "cloning rfid reader"
+git clone https://github.com/jamieboyd/RFIDTagReader.git
+cd RFIDTagReader
+python3 RFIDTagReader_setup.py install
+cd ..
+
+
+echo "cloning adafruit python mpr121"
+git clone https://github.com/adafruit/Adafruit_Python_MPR121.git
+cd Adafruit_Python_MPR121
+sudo python3 setup.py install
+cd .. 
+
+
+echo "cloning adafruit GPIO"
+git clone https://github.com/adafruit/Adafruit_Python_GPIO.git 
+cd Adafruit_Python_GPIO
+sudo python3 setup.py install
+cd ..
+
+
+echo "cloning touch detector"
+git clone https://github.com/Judge24601/TouchDetector.git
+cd TouchDetector
+python3 TouchDetector_setup.py install
+cd ..
+
+
+
+echo "installing pypy and remaining modules (mysql-server, php-mysql, pymysql)"
+sudo apt-get install pypy mysql-server php-mysql -y
+python3 -m pip install PyMySQL 
+
+
+echo "setting up database"
+
+sudo apt-get install libatlas-base-dev -y 
+sudo apt-get install python3-scipy -y
+sudo pip3 install imreg_dft
+sudo pip3 install matplotlib
+sudo pip3 install pynput
+sudo pip3 install h5py
+sudo pip3 install pandas 
+sudo pip3 install h5py #Not a typo. Sometimes needs to be run twice
+sudo apt-get install libhdf5-dev -y
+
+
+
+
+
+
+
