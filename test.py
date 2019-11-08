a2105c75bfe09590686e97a0cd1c98169267674c
tf4ong <53628964+tf4ong@users.noreply.github.com>
2019-11-08 11:50:54 -0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,42 @@
+import RPi.GPIO as GPIO
+import time
+selenoid1_pin= 17 
+selenoid2_pin=27
+selenoid3_pin=21
+selenoid4_pin=25
+class selenoid:
+	def __init__(self, pin):
+		selenoid.pin=pin
+	def activate(self,open_time):
+		GPIO.setmode(GPIO.BCM)
+		GPIO.setwarnings(False)
+		GPIO.setup(selenoid.pin,GPIO.OUT)
+		GPIO.output(selenoid.pin,GPIO.HIGH)
+		time.sleep(int(open_time))
+		GPIO.output(selenoid.pin,GPIO.LOW)
+		GPIO.cleanup()
+
+'''
+
+def turnvalve(value=1):
+	GPIO.setmode(GPIO.BCM)
+	GPIO.setwarnings(False)
+	if value ==1 :
+		GPIO.setup(servo1_pin,GPIO.OUT)
+		GPIO.output(servo1_pin, GPIO.HIGH)
+		time.sleep(1)
+		GPIO.output(servo1_pin, GPIO.LOW)
+		GPIO.cleanup()
+	elif value ==2:
+		GPIO.setup(servo2_pin,GPIO.OUT)
+		GPIO.output(servo2_pin, GPIO.HIGH)
+		time.sleep(1)
+		GPIO.output(servo2_pin, GPIO.LOW)
+		GPIO.cleanup()
+	else:
+		print('retry')
+	
+if __name__== '__main__':
+	value=input('1 or 2')
+	turnvalve(int(value))
+'''
