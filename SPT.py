fb0df20c2c762d5ee25828c3693aaf6b30804fc7
tf4ong <53628964+tf4ong@users.noreply.github.com>
2019-11-25 16:08:25 -0800

    Update SPT.py

--- SPT.py
+++ SPT.py
@@ -1,81 +1,43 @@
-from time import time, sleep
+import RPi.GPIO as GPIO
+from time import sleep
 import board
 import busio
-from SPT import SPT
-import RFIDTagReader
-from RFIDTagReader import TagReader
-import RPi.GPIO as GPIO 
 import datetime as dt
-import adafruit_mpr121 as mpr121
-'''
-Default variables
-'''
-serialPort = '/dev/ttyUSB0'
-tag_in_range_pin=18
-selenoid_pin_LW=26
-selenoid_pin_LS=13
-selenoid_pin_RW=21
-selenoid_pin_RS=12
-i2c=busio.I2C(board.SCL,board.SDA)
-lickdector=mpr121.MPR121(i2c,address=0x5A)
-selenoid_RW=SPT.selenoid(selenoid_pin_RW)
-selenoid_RS=SPT.selenoid(selenoid_pin_RS)
-selenoid_LW=SPT.selenoid(selenoid_pin_LS)
-selenoid_LS=SPT.selenoid(selenoid_pin_LW)
-globalReader = None
-globalTag = 0
-mice={801010565:{'SPT_level':1,'SPT_pattern':'R'}}
-"""
-Main loop for SPT 
-Need to add camera and scale in loop
-Need to think of open design for tunnel
-"""
-def main ():
-    global globalReader
-    global globalTag
-    globalReader = TagReader(serialPort, True, timeOutSecs = 0.05, kind='ID')
-    globalReader.installCallback (tag_in_range_pin)
-    cage=input('cage?')
-    txtspacer=input('txt spacer?')
-    log=SPT.data_logger(cage,txtspacer)
-    print ("Waiting for mouse....")
-    while True:
-        try:
-            while RFIDTagReader.globalTag == 0:
-                sleep (0.02)
-            tag = RFIDTagReader.globalTag
-            log.event_outcome(mice,str(tag),'Entered')
-            if mice[tag]['SPT_level']== 0:
-                selenoid_RW.activate(0.5)
-                log.event_outcome(mice,str(tag),'Entry_Reward')
-                pass
-            while RFIDTagReader.globalTag == tag:
-                while GPIO.input(tag_in_range_pin) == GPIO.HIGH:
-                    if lickdector[0].value:
-                        log.event_outcome(mice,str(tag),'licked-Rightside')
-                        if mice[tag]['SPT_pattern']=='R':
-                            selenoid_LW.activate(0.2)
-                            log.event_outcome(mice,str(tag),'Sucrose_Reward')
-                        elif mice[tag]['SPT_pattern']=='L':
-                            selenoid_RW.activate(0.2)
-                            sleep(0.02)
-                            log.event_outcome(mice,str(tag),'Water_Reward')
-                    elif lickdector[1].value:
-                        log.event_outcome(mice,str(tag),'licked-Leftside')
-                        if mice[tag]['SPT_pattern']=='R':
-                            selenoid_LW.activate(0.2)
-                            log.event_outcome(mice,str(tag),'Water_Reward')
-                        elif mice[tag]['SPT_pattern']=='L':
-                            selenoid_LS.activate(0.2)
-                            log.event_outcome(mice,str(tag),'Sucrose_Reward')
-                    else:
-                        sleep(0.02)
-            sleep(0.02)
-            log.event_outcome(mice,str(tag),'Exit')
-            print('Waiting for mouse')
-        except KeyboardInterrupt:
-            del globalReader
-            print ("Quitting")
-            break
-if __name__ == '__main__':
-   main()
+
+import os
+class selenoid:
+	def __init__(self, pin):
+		self.pin=pin
+		GPIO.setup(self.pin,GPIO.OUT)
+	def activate(self,open_time):
+		GPIO.setwarnings(False)
+		GPIO.output(self.pin,GPIO.HIGH)
+		sleep(float(open_time))
+		GPIO.output(self.pin,GPIO.LOW)
+		sleep(0.05)		
+class data_logger:
+	def __init__(self,cage,txtspacer):
+		self.cage=cage
+		self.txtspacer=txtspacer
+		today=dt.datetime.now().strftime('%Y-%m-%d')
+		self.filename=self.cage+'/'+'SPT_'+today+'_'+self.cage+'.csv'
+		if not os.path.exists(cage+'/'):
+			os.mkdir(self.cage+'/')
+			print(cage+' Dictionary created')
+		else:
+			pass
+		if not os.path.exists(self.filename):
+			print('First day starting SPT')
+			with open(self.filename,'a') as file:
+				file.write('Time,Tag,Surcose_Pattern,Event\n')
+		else:
+			pass
+	def event_outcome(self,mice,mouse,x):
+		spacer='\t'
+		sucrose_pattern=mice[int(mouse)]['SPT_pattern']
+		current_time=dt.datetime.now().strftime('%Y-%m-%d %H-%M-%S')
+		Event= current_time+spacer+mouse+spacer+x
+		with open(self.filename,'a') as file:
+			file.write(dt.datetime.now().strftime('%Y-%m-%d %H-%M-%S')+self.txtspacer+mouse+self.txtspacer+sucrose_pattern+self.txtspacer+x+'\n')
+		print(Event)
+		
