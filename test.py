fe35dd6c00c466e4a5ec6f0195b50b96494d8acb
tf4ong <53628964+tf4ong@users.noreply.github.com>
2019-11-25 16:00:16 -0800

    Update test.py

--- test.py
+++ test.py
@@ -1,42 +1,82 @@
-import RPi.GPIO as GPIO
-import time
-selenoid1_pin= 17 
-selenoid2_pin=27
-selenoid3_pin=21
-selenoid4_pin=25
-class selenoid:
-	def __init__(self, pin):
-		selenoid.pin=pin
-	def activate(self,open_time):
-		GPIO.setmode(GPIO.BCM)
-		GPIO.setwarnings(False)
-		GPIO.setup(self.pin,GPIO.OUT)
-		GPIO.output(self.pin,GPIO.HIGH)
-		time.sleep(int(open_time))
-		GPIO.output(self.pin,GPIO.LOW)
-	@staticmethod 
-	def clean_up(self):
-		GPIO.cleanup()
+from time import time, sleep
+from SPT import SPT
+import RFIDTagReader
+from RFIDTagReader import TagReader
+import RPi.GPIO as GPIO 
+import datetime as dt
 '''
-def turnvalve(value=1):
-	GPIO.setmode(GPIO.BCM)
-	GPIO.setwarnings(False)
-	if value ==1 :
-		GPIO.setup(servo1_pin,GPIO.OUT)
-		GPIO.output(servo1_pin, GPIO.HIGH)
-		time.sleep(1)
-		GPIO.output(servo1_pin, GPIO.LOW)
-		GPIO.cleanup()
-	elif value ==2:
-		GPIO.setup(servo2_pin,GPIO.OUT)
-		GPIO.output(servo2_pin, GPIO.HIGH)
-		time.sleep(1)
-		GPIO.output(servo2_pin, GPIO.LOW)
-		GPIO.cleanup()
-	else:
-		print('retry')
-	
-if __name__== '__main__':
-	value=input('1 or 2')
-	turnvalve(int(value))
+Default variables
 '''
+serialPort = '/dev/ttyUSB0'
+tag_in_range_pin=18
+selenoid_pin_LW=26
+selenoid_pin_LS=13
+selenoid_pin_RW=21
+selenoid_pin_RS=LED(12)
+i2c=busio.I2C(board.SCL,board.SDA)
+lickdector=mpr121.MPR121(i2c,address=0x5A)
+selenoid_RW=SPT.selenoid(selenoid_pin_RW)
+#selenoid_RS=SPT.selenoid(selenoid_pin_RS)
+selenoid_LW=SPT.selenoid(selenoid_pin_LS)
+selenoid_LS=SPT.selenoid(selenoid_pin_LW)
+globalReader = None
+globalTag = 0
+mice={801010565:{'SPT_level':1,'SPT_pattern':'R'}}
+"""
+Main loop for SPT 
+Need to add camera and scale in loop
+Need to think of open design for tunnel
+"""
+def main ():
+    global globalReader
+    global globalTag
+    globalReader = TagReader(serialPort, True, timeOutSecs = 0.05, kind='ID')
+    globalReader.installCallback (tag_in_range_pin)
+    cage=input('cage?')
+    txtspacer=input('txt spacer?')
+    log=SPT.data_logger(cage,txtspacer)
+    print ("Waiting for mouse....")
+    while True:
+        try:
+            while RFIDTagReader.globalTag == 0:
+                sleep (0.02)
+            tag = RFIDTagReader.globalTag
+            log.event_outcome(mice,str(tag),'Entered')
+            if mice[tag]['SPT_level']== 0:
+                selenoid_RW.activate(0.5)
+                log.event_outcome(mice,str(tag),'Entry_Reward')
+                pass
+            while RFIDTagReader.globalTag == tag:
+                while GPIO.input(tag_in_range_pin) == GPIO.HIGH:
+                    if lickdector[0].value:
+                        log.event_outcome(mice,str(tag),'licked-Rightside')
+                        if mice[tag]['SPT_pattern']=='R':
+                            selenoid_LW.activate(0.2)
+                            log.event_outcome(mice,str(tag),'Sucrose_Reward')
+                            log.event_outcome(mice,str(tag),(str(tag)))
+                        elif mice[tag]['SPT_pattern']=='L':
+                            selenoid_RW.activate(0.2)
+                            sleep(0.02)
+                            log.event_outcome(mice,str(tag),'Water_Reward')
+                            log.event_outcome(mice,str(tag),(str(tag)))
+                    elif lickdector[1].value:
+                        log.event_outcome(mice,str(tag),'licked-Leftside')
+                        if mice[tag]['SPT_pattern']=='R':
+                            selenoid_LW.activate(0.2)
+                            log.event_outcome(mice,str(tag),'Water_Reward')
+                            log.event_outcome(mice,str(tag),(str(tag)))
+                        elif mice[tag]['SPT_pattern']=='L':
+                            selenoid_LS.activate(0.2)
+                            log.event_outcome(mice,str(tag),'Sucrose_Reward')
+                            log.event_outcome(mice,str(tag),(str(tag)))
+                    else:
+                        sleep(0.02)
+            sleep(0.02)
+            log.event_outcome(mice,str(tag),'Exit')
+            print('Waiting for mouse')
+        except KeyboardInterrupt:
+            del globalReader
+            print ("Quitting")
+            break
+if __name__ == '__main__':
+   main()
