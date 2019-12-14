29f0ace48364509e28ce44425a017fadc028be72
tf4ong <53628964+tf4ong@users.noreply.github.com>
2019-12-13 17:14:28 -0800

    Update SPT.py

--- SPT.py
+++ SPT.py
@@ -11,18 +11,19 @@ from picamera import PiCamera
 import os
 class selenoid:
 	def __init__(self, pin):
+		GPIO.setmode(GPIO.BCM)
 		self.pin=pin
 		GPIO.setup(self.pin,GPIO.OUT)
 	def activate(self,open_time):
 		GPIO.setwarnings(False)
 		GPIO.output(self.pin,GPIO.HIGH)
 		sleep(float(open_time))
-		GPIO.output(self.pin,GPIO.LOW)	
+		GPIO.output(self.pin,GPIO.LOW)
 class data_logger:
 	def __init__(self,cage,txtspacer):
 		self.cage=cage
 		self.txtspacer=txtspacer
-		today=dt.datetime.now().strftime('%Y-%m-%d')
+		today=dt.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
 		self.filename=self.cage+'/'+'SPT_'+today+'_'+self.cage+'.csv'
 		if not os.path.exists(cage+'/'):
 			os.mkdir(self.cage+'/')
@@ -30,25 +31,26 @@ class data_logger:
 		else:
 			pass
 		if not os.path.exists(self.filename):
-			print('First day starting SPT')
+			print('Starting a new day starting SPT')
 			with open(self.filename,'a') as file:
-				file.write('Time,Tag,Surcose_Pattern,Event,Event_dict\n')
+				file.write('Time,Tag,Surcose_Pattern,SPT_level,Event,Event_dict\n')
 		else:
 			pass
 	def event_outcome(self,mice,mouse,event,event_dict):
-		spacer='\t'
+		spacer="    "
 		sucrose_pattern=mice[int(mouse)]['SPT_pattern']
-		current_time=dt.datetime.now().strftime('%Y-%m-%d %H-%M-%S')
-		Event= current_time+spacer+mouse+spacer+event+spacer+event_dict
+		spt_level=str(mice[int(mouse)]['SPT_level'])
+		current_time=dt.datetime.now().strftime('%Y-%m-%d %H-%M-%S.%f')[:-3]
+		Event= current_time+spacer+mouse+spacer+sucrose_pattern+spacer+spt_level+spacer+event+spacer+event_dict
 		with open(self.filename,'a') as file:
-			file.write(dt.datetime.now().strftime('%Y-%m-%d %H-%M-%S')+self.txtspacer+mouse+self.txtspacer+sucrose_pattern+self.txtspacer+event+event_dict+'\n')
-		print(Event)
+			file.write(dt.datetime.now().strftime('%Y-%m-%d %H-%M-%S')+self.txtspacer+mouse+self.txtspacer+sucrose_pattern+self.txtspacer+spt_level+self.txtspacer+event+self.txtspacer+event_dict+'\n')
+		print(Event+'\n')
 class piVideoStream:
 	def __init__(self,folder,resolution=(640,480),framerate=90,vidformat='h264',quality=25,preview=(0,0,640,480)):
 		self.cam=PiCamera()
 		self.resolution=resolution
 		self.framerate=framerate
-		self.format=vidformat
+		self.vidformat=vidformat
 		self.quality=quality
 		self.preview=preview
 		self.folder= folder
@@ -64,4 +66,18 @@ class piVideoStream:
 	def stop_record(self):
 		self.cam.stop_preview()
 		self.cam.stop_recording()
-		#self.cam.close()
+class buzzer:
+	def __init__(self,pin,pitch,times):
+		GPIO.setmode(GPIO.BCM)
+		self.pin=pin
+		self.delay=(1/pitch)/2
+		self.cycle=times
+		GPIO.setup(self.pin,GPIO.OUT)
+	def buzz(self):
+		for i in range(self.cycle):
+			GPIO.output(self.pin, True)
+			sleep(self.delay)
+			GPIO.output(self.pin, False)
+			sleep(self.delay)
+		
+				
