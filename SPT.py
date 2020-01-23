9c4bf36275d5e3f1a4099a58b0f6bed5aba68796
tf4ong <53628964+tf4ong@users.noreply.github.com>
2020-01-22 17:13:49 -0800

    Update SPT.py

--- SPT.py
+++ SPT.py
@@ -11,75 +11,75 @@ from picamera import PiCamera
 import os
 import json
 class selenoid:
-    def __init__(self, pin):
-        GPIO.setmode(GPIO.BCM)
-        self.pin=pin
-        GPIO.setup(self.pin,GPIO.OUT)
-    def activate(self,open_time):
-        GPIO.setwarnings(False)
-        GPIO.output(self.pin,GPIO.HIGH)
-        sleep(float(open_time))
-        GPIO.output(self.pin,GPIO.LOW)
+	def __init__(self, pin):
+		GPIO.setmode(GPIO.BCM)
+		self.pin=pin
+		GPIO.setup(self.pin,GPIO.OUT)
+	def activate(self,open_time):
+		GPIO.setwarnings(False)
+		GPIO.output(self.pin,GPIO.HIGH)
+		sleep(float(open_time))
+		GPIO.output(self.pin,GPIO.LOW)
 class data_logger:
-    def __init__(self,cage,txtspacer):
-        self.cage=cage
-        self.txtspacer=txtspacer
-        today=dt.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
-        self.filename=self.cage+'/'+'SPT_'+today+'_'+self.cage+'.csv'
-        if not os.path.exists(cage+'/'):
-            os.mkdir(self.cage+'/')
-            print(cage+' Dictionary created')
-        else:
-            pass
-        if not os.path.exists(self.filename):
-            print('Starting a new day starting SPT')
-            with open(self.filename,'a') as file:
-                file.write('Time,Tag,Surcose_Pattern,SPT_level,Event,Event_dict\n')
-        else:
-            pass
-    def event_outcome(self,mice,mouse,event,event_dict):
-        spacer="    "
-        sucrose_pattern=mice[int(mouse)]['SPT_pattern']
-        spt_level=str(mice[int(mouse)]['SPT_level'])
-        current_time=dt.datetime.now().strftime('%Y-%m-%d %H-%M-%S.%f')[:-3]
-        Event= current_time+spacer+mouse+spacer+sucrose_pattern+spacer+spt_level+spacer+event+spacer+event_dict
-        with open(self.filename,'a') as file:
-            file.write(dt.datetime.now().strftime('%Y-%m-%d %H-%M-%S')+self.txtspacer+mouse+self.txtspacer+sucrose_pattern+self.txtspacer+spt_level+self.txtspacer+event+self.txtspacer+event_dict+'\n')
-        print(Event+'\n')
+	def __init__(self,cage,txtspacer):
+		self.cage=cage
+		self.txtspacer=txtspacer
+		today=dt.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+		self.filename=self.cage+'/'+'SPT_'+today+'_'+self.cage+'.csv'
+		if not os.path.exists(cage+'/'):
+			os.mkdir(self.cage+'/')
+			print(cage+' Dictionary created')
+		else:
+			pass
+		if not os.path.exists(self.filename):
+			print('Starting a new day starting SPT')
+			with open(self.filename,'a') as file:
+				file.write('Time,Tag,Surcose_Pattern,SPT_level,Event,Event_dict\n')
+		else:
+			pass
+	def event_outcome(self,mice,mouse,event,event_dict):
+		spacer="    "
+		sucrose_pattern=mice[int(mouse)]['SPT_pattern']
+		spt_level=str(mice[int(mouse)]['SPT_level'])
+		current_time=dt.datetime.now().strftime('%Y-%m-%d %H-%M-%S.%f')[:-3]
+		Event= current_time+spacer+mouse+spacer+sucrose_pattern+spacer+spt_level+spacer+event+spacer+event_dict
+		with open(self.filename,'a') as file:
+			file.write(dt.datetime.now().strftime('%Y-%m-%d %H-%M-%S')+self.txtspacer+mouse+self.txtspacer+sucrose_pattern+self.txtspacer+spt_level+self.txtspacer+event+self.txtspacer+event_dict+'\n')
+		print(Event+'\n')
 class piVideoStream:
-    def __init__(self,folder,resolution=(640,480),framerate=90,vidformat='h264',quality=25,preview=(0,0,640,480)):
-        self.cam=PiCamera()
-        self.resolution=resolution
-        self.framerate=framerate
-        self.vidformat=vidformat
-        self.quality=quality
-        self.preview=preview
-        self.folder= folder
-    def cam_setup(self):
-        self.cam.resolution=self.resolution
-        self.cam.framerate=self.framerate 
-    def record(self,tag):
-        self.cam.start_preview(fullscreen=False,window=self.preview)
-        self.filename=str(tag)+'_'+dt.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')+'.h264' 
-        self.filepath=self.folder+self.filename
-        self.cam.start_recording(self.filepath,quality=self.quality)
-        return self.filename
-    def stop_record(self):
-        self.cam.stop_preview()
-        self.cam.stop_recording()
+	def __init__(self,folder,resolution=(640,480),framerate=90,vidformat='h264',quality=25,preview=(0,0,640,480)):
+		self.cam=PiCamera()
+		self.resolution=resolution
+		self.framerate=framerate
+		self.vidformat=vidformat
+		self.quality=quality
+		self.preview=preview
+		self.folder= folder
+	def cam_setup(self):
+		self.cam.resolution=self.resolution
+		self.cam.framerate=self.framerate 
+	def record(self,tag):
+		self.cam.start_preview(fullscreen=False,window=self.preview)
+		self.filename=str(tag)+'_'+dt.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')+'.h264' 
+		self.filepath=self.folder+self.filename
+		self.cam.start_recording(self.filepath,quality=self.quality)
+		return self.filename
+	def stop_record(self):
+		self.cam.stop_preview()
+		self.cam.stop_recording()
 class buzzer:
-    def __init__(self,pin,pitch,times):
-        GPIO.setmode(GPIO.BCM)
-        self.pin=pin
-        self.delay=(1/pitch)/2
-        self.cycle=times
-        GPIO.setup(self.pin,GPIO.OUT)
-    def buzz(self):
-        for i in range(self.cycle):
-            GPIO.output(self.pin, True)
-            sleep(self.delay)
-            GPIO.output(self.pin, False)
-            sleep(self.delay)
+	def __init__(self,pin,pitch,times):
+		GPIO.setmode(GPIO.BCM)
+		self.pin=pin
+		self.delay=(1/pitch)/2
+		self.cycle=times
+		GPIO.setup(self.pin,GPIO.OUT)
+	def buzz(self):
+		for i in range(self.cycle):
+			GPIO.output(self.pin, True)
+			sleep(self.delay)
+			GPIO.output(self.pin, False)
+			sleep(self.delay)
 '''
 need to integrate into main as class
 '''
@@ -97,7 +97,7 @@ class mice_dict:
         self.config_file_path=cage+'/'+'SPT_mouse_config.jsn'
         if not os.path.exists(self.config_file_path):
             print('No previoues mice configurations found')
-            pass
+            self.startup()
         else:
             with open(self.config_file_path,'r') as file:
                 self.mice_config=json.loads(file.read().replace('\n',',')) 
@@ -114,41 +114,43 @@ class mice_dict:
                 i+=1
             print('All mice '+str(numMice)+' added')
     def add_mice(self):
-        global tagreader
         try:
             tagreader = TagReader (RFID_serialPort, RFID_doCheckSum, timeOutSecs = RFID_timeout, kind=RFID_kind)
         except Exception as e:
-            raise e
+                raise e
         i=0
+        print('Scan mice now')
         while i<1:
             try:
-                tag = tagReader.readTag ()
+                tag = tagreader.readTag ()
                 i+=1
                 print (tag)
             except ValueError as e:
                 print (str (e))
-        tagReader.clearBuffer()
+        tagreader.clearBuffer()
         SPT_lvl=input('Enter SPT level for new mouse:')
         SPT_Spout=input('Enter initial SPT spout for new mouse (R/L):')
         temp={tag:{'SPT_Spout': SPT_Spout, 'SPT_level': int(SPT_lvl)}}
-        mice. update(temp)
-    def write_log_config():
-        if not os.path.exists():
-            with open('SPT_mouse_past_config.csv','w') as file:
+        self.mice_config.update(temp)
+    def write_log_config(self):
+        if not os.path.exists(self.cage+'/'+'SPT_mouse_past_config.csv'):
+            with open(self.cage+'/'+'SPT_mouse_past_config.csv','w') as file:
                 file.write('Date,Tag,SPT_level,SPT_Spout\n')
+            log_date=dt.datetime.now().strftime('%Y-%m-%d %H-%M-%S')
+            for k, v in self.mice_config.items():
+                file.write(log_date+','+str(k)+','+str(v['SPT_level'])+','+v['SPT_Spout']+'\n')
         else:
-            with open('SPT_mouse_past_config.csv','a') as file:
+            with open(self.cage+'/'+'SPT_mouse_past_config.csv','a') as file:
                log_date=dt.datetime.now().strftime('%Y-%m-%d %H-%M-%S')
                for k, v in self.mice_config.items():
                    file.write(log_date+','+str(k)+','+str(v['SPT_level'])+','+v['SPT_Spout']+'\n')
-    def spout_swtich():
+    def spout_swtich(self):
         self.write_log_config()
         for k, v in self.mice_config.items():
-            if mice[k]['SPT_Spout'] == 'R':
-                mice[k]['SPT_Spout']='L'
-            elif mice[k]['SPT_Spout']=='L':
-                mice[k]['SPT_Spout']='R'
-
+            if self.mice_config[k]['SPT_Spout'] == 'R':
+                self.mice_config[k]['SPT_Spout']='L'
+            elif self.mice_config[k]['SPT_Spout']=='L':
+                self.mice_config[k]['SPT_Spout']='R'
 ###############
 class task_settings():
     def __init__(self,task_name):
@@ -193,4 +195,3 @@ class task_settings():
         temp= json.dumps(self.task_config).replace(',','\n')
         with open ('SPT_'+self.task_name+'.jsn','w') as outfile:
             outfile.write(temp)
-
